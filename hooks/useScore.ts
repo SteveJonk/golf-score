64e3db353125308f285e067d4193ac50671066c6
Steve Jonk <stevejonk1993@gmail.com>
2024-03-17 16:10:59 +0100

    feat: recalculate score based on rounds

--- hooks/useScore.ts
+++ hooks/useScore.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Score, getStartScore } from '@/data/score'
 import Cookie from 'js-cookie'
 
@@ -15,13 +15,22 @@ export const useScore = (savedScore: Score[]) => {
   const [rounds, setRounds] = useState(9)
   const [score, setScore] = useState(savedScore || getStartScore())
 
+  useEffect(() => {
+    const recalculateScore = (score: Score[]) => {
+      return score.map((player) => {
+        player.total = player.score.slice(0, rounds).reduce((acc, score) => acc + score, 0)
+        return player
+      })
+    }
+    setScore((prevScore) => recalculateScore(prevScore))
+  }, [rounds])
+
   const changeScore = (newScore: ChangeScore) => {
     const newScoreArray = [...score]
     newScoreArray[newScore.playerIndex].score[newScore.roundIndex] = newScore.value
-    newScoreArray[newScore.playerIndex].total = newScoreArray[newScore.playerIndex].score.reduce(
-      (acc: number, score: number) => acc + score,
-      0
-    )
+    newScoreArray[newScore.playerIndex].total = newScoreArray[newScore.playerIndex].score
+      .slice(0, rounds)
+      .reduce((acc: number, score: number) => acc + score, 0)
     setScore(newScoreArray)
     Cookie.set('golf_score', JSON.stringify(newScoreArray))
   }
